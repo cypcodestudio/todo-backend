ca3dfceb0104c124b0cec3f4bfea3b89d91771bb
cypcode <147054372+cypcodestudio@users.noreply.github.com>
2026-06-06 13:04:01 +0200

    allow cors origins all

--- src/main/java/com/cypcode/todo/controller/TagsController.java
+++ src/main/java/com/cypcode/todo/controller/TagsController.java
@@ -4,12 +4,14 @@ import com.cypcode.todo.api.TagsApi;
 import com.cypcode.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class TagsController implements TagsApi {
